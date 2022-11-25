36db44ed7dbf371515ac885d2fd84e69eb43d95a
Rhenald <renaldkarel@gmail.com>
2022-11-25 11:21:44 +0700

    add pending UI

--- apps/01-before/app/routes/resources/customers.tsx
+++ apps/01-before/app/routes/resources/customers.tsx
@@ -35,12 +35,13 @@ export function CustomerCombobox({ error }: { error?: string | null}) {
     itemToString: item => (item ? item.name : ''),
     onInputValueChange: changes => {
       customerFetcher.submit(
-        { query: changes.inputValue },
+        { query: changes.inputValue ?? '' },
         { method: 'get', action: '/resources/customers'}
       );
     },
   })
 
+  const showSpinner = customerFetcher.state !== 'idle';
   const displayMenu = cb.isOpen && customers.length > 0;
 
   return (
@@ -71,7 +72,7 @@ export function CustomerCombobox({ error }: { error?: string | null}) {
             'aria-errormessage': error ? 'customer-error' : undefined,
           })}
         />
-        {/* 🐨 render spinner here */}
+        <Spinner showSpinner={showSpinner} />
       </div>
       <ul
         {...cb.getMenuProps({
@@ -95,6 +96,39 @@ export function CustomerCombobox({ error }: { error?: string | null}) {
             ))
           : null}
       </ul>
+    </div>
+  )
+}
+
+function Spinner({ showSpinner }: { showSpinner: boolean }) {
+  return (
+    <div
+      className={`absolute right-0 top-[6px] transition-opacity ${
+        showSpinner ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <svg
+        className="-ml-1 mr-3 h-5 w-5 animate-spin"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        width="1em"
+        height="1em"
+      >
+        <circle
+          className="opacity-25"
+          cx={12}
+          cy={12}
+          r={10}
+          stroke="currentColor"
+          strokeWidth={4}
+        />
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        />
+      </svg>
     </div>
   )
 }
