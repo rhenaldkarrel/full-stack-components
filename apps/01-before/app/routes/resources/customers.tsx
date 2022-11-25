70af5277099b1984df4236f6d8a4338be0c703f8
Rhenald <renaldkarel@gmail.com>
2022-11-25 11:05:03 +0700

    feat: create the resource route

--- apps/01-before/app/routes/resources/customers.tsx
+++ apps/01-before/app/routes/resources/customers.tsx
@@ -1,3 +1,15 @@
-// 🐨 finish the owl
+import type { LoaderArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import invariant from 'tiny-invariant';
+import { searchCustomers } from '~/models/customer.server';
+import { requireUser } from '~/session.server';
 
-export {}
+export async function loader({ request }: LoaderArgs) {
+  await requireUser(request);
+  const url = new URL(request.url);
+  const query = url.searchParams.get('query');
+  invariant(typeof query === 'string', 'query is required')
+  return json({
+    customers: await searchCustomers(query),
+  });
+}
